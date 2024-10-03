4ede241e5e7070435f3fa257f2d7d7eb2b8fc894
younyikim <rlakuku1221@gmail.com>
2024-10-03 20:18:23 +0900

    practice

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import { inter } from '@/app/ui/font';
+import '@/app/ui/global.css';
+
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    );
 }
